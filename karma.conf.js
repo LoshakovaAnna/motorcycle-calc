6bee089c252141c12a07129c2556cfd896c6ac1c
anna.loshakova <anna.loshakova@specific-group.com>
2019-12-04 17:13:21 +0300

    codeCoverage

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: path.join(__dirname, './coverage/motor-calc'),
+      dir: path.join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
